260caeb41056c34afe8bb08c9d2f9be7dcd9984a
Nick Rosier <github@bunbun.be>
2020-12-29 23:48:09 +0100

    Send sonar data to 2nd Microbot

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let sonar = 0
+radio.setGroup(1)
 basic.showLeds(`
     . . . # .
     # . # . #
@@ -7,24 +8,19 @@ basic.showLeds(`
     . # . . .
     `)
 basic.pause(2000)
-basic.clearScreen()
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(50, 25)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(25, 50)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.motors(50, 50)
+    }
+})
 basic.forever(function () {
     sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    led.plotBarGraph(
-    sonar,
-    50
-    )
-    if (sonar >= 5) {
-        if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-            cuteBot.motors(50, 25)
-        }
-        if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-            cuteBot.motors(25, 50)
-        }
-        if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
-            cuteBot.motors(50, 50)
-        }
-    } else {
-        cuteBot.motors(0, 0)
-    }
+    radio.sendValue("sonar", sonar)
+    basic.pause(1000)
 })
