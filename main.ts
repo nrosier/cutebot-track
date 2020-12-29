c74fb13687fbcfc1bb682c7de2ab114c0aa6624d
Nick Rosier <github@bunbun.be>
2020-12-29 23:58:14 +0100

    Emergency breaks

--- main.ts
+++ main.ts
@@ -9,18 +9,22 @@ basic.showLeds(`
     `)
 basic.pause(2000)
 basic.forever(function () {
-    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-        cuteBot.motors(50, 25)
-    }
-    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-        cuteBot.motors(25, 50)
-    }
-    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
-        cuteBot.motors(50, 50)
+    if (sonar > 10) {
+        if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+            cuteBot.motors(50, 25)
+        }
+        if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+            cuteBot.motors(25, 50)
+        }
+        if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+            cuteBot.motors(50, 50)
+        }
+    } else {
+        cuteBot.stopcar()
     }
 })
 basic.forever(function () {
     sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     radio.sendValue("sonar", sonar)
-    basic.pause(1000)
+    basic.pause(100)
 })
